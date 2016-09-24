3858b37d3921000d11fef499f74bf40126f7e4c6
happy88 <happychallenge88@gmail.com>
2016-09-24 17:01:54 +0800

    Some function

--- interest/models.py
+++ interest/models.py
@@ -11,21 +11,48 @@ class Publisher(models.Model):
  
     def __str__(self):
         return self.name
- 
+
+
+class MaleManager(models.Manager):
+	def get_queryset(self):
+		return super(MaleManager, self).get_queryset().filter(sex='M')
+
+class FemaleManager(models.Manager):
+	def get_queryset(self):
+		return super(FemaleManager, self).get_queryset().filter(sex='F')
+
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
+    sex = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
+    birthd = models.DateField()
+    people = models.Manager()
+    men = MaleManager()
+    women = FemaleManager()
  
     def __str__(self):
         return '%s %s' % (self.first_name, self.last_name)
+
  
+class BookManager(models.Manager):
+	def title_count(self, keyword):
+		return self.filter(title__icontains=keyword).count()
+
+
+class NameBookManager(models.Manager):
+	def get_queryset(self):
+		return super(NameBookManager, self).get_queryset().filter(author='first_name1 last_name1')
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField()
+    num_pages = models.IntegerField(blank=True, null=True)
+    objects = BookManager()
+    nameobjects = NameBookManager()
  
     def __str__(self):
         return self.title
